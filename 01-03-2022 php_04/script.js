db99f5d0ae86e764f3a9cc9f871513d833b4bd24
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-03-01 20:34:36 +0530

    if

--- 01-03-2022 php_04/script.js
+++ 01-03-2022 php_04/script.js
@@ -2,6 +2,11 @@ function signIn() {
     var email = document.getElementById("e");
     var password = document.getElementById("p");
 
-    alert(email.value);
-    alert(password.value);
+    // alert(email.value);
+    // alert(password.value);
+
+    if (email == "admin@gmail.com" & password == "Admin123") {
+        alert("Success");
+        window.location("home.php");
+    }
 }
