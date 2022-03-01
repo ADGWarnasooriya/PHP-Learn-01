fd7919b938da911eddc2f347f6f681ec38dda7f4
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-03-01 20:00:13 +0530

    html

---
+++ 01-03-2022 php_04/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>PHP 4</title>
+</head>
+
+<body>
+
+    <span>Email</span>
+    <input type="text">
+
+    <br><br>
+
+    <span>Password</span>
+    <input type="password">
+
+    <br><br>
+
+    <button>Sign In</button>
+
+
+</body>
+
+</html>
