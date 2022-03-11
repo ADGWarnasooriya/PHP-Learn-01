51611210e99f233a8023ba81c9c16cb060dbea7b
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-03-11 20:48:31 +0530

    input class="field"

--- 11-03-2022 php_05/index.php
+++ 11-03-2022 php_05/index.php
@@ -15,22 +15,22 @@
         <h1 class="topic">Create A New Account</h1>
 
         <span class="txt">First name</span><br>
-        <input type="text"><br><br>
+        <input class="field" type="text"><br><br>
 
         <span class="txt">Last name</span><br>
-        <input type="text"><br><br>
+        <input class="field" type="text"><br><br>
 
         <span class="txt">Email</span><br>
-        <input type="text"><br><br>
+        <input class="field" type="text"><br><br>
 
         <span class="txt">Mobile</span><br>
-        <input type="text"><br><br>
+        <input class="field" type="text"><br><br>
 
         <span class="txt">Password</span><br>
-        <input type="text"><br><br>
+        <input class="field" type="text"><br><br>
 
         <span class="txt">City</span><br>
-        <input type="text"><br><br>
+        <input class="field" type="text"><br><br>
 
         <button>Create Account</button>
 
