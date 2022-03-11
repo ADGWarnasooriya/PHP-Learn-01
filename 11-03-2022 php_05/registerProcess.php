da2f18c14545fde087eb373d3095fa95b34143f8
ADGWarnasooriya <phoenixfire696@gmail.com>
2022-03-11 22:09:20 +0530

    php

--- 11-03-2022 php_05/registerProcess.php
+++ 11-03-2022 php_05/registerProcess.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "Hello";
+$fname = $_GET["fname"];
+$lname = $_GET["lname"];
+$email = $_GET["email"];
+$mobile = $_GET["mobile"];
+$password = $_GET["pw"];
+$city = $_GET["city"];
+
+echo $fname;
+echo "</br>";
+echo $lname;
+echo "</br>";
+echo $email;
+echo "</br>";
+echo $mobile;
+echo "</br>";
+echo $password;
+echo "</br>";
+echo $city;
+echo "</br>";
